eeacd11f1fca426da88c36e1f5a43f61f60928f9
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-04-03 18:06:46 -0500

    Filas

--- Preinforme9_SantiagoDelvalle_Jhoncorzo.py
+++ Preinforme9_SantiagoDelvalle_Jhoncorzo.py
@@ -1,21 +1,10 @@
-print("Ejercicio 3")
+print("Ejercicio 4")
 
-n1 = float(input("Digite el valor de la primera nota: "))
-n2 = float(input("Digite el valor de la segunda nota: "))
-n3 = float(input("Digite el valor de la tercera nota: "))
-n4 = float(input("Digite el valor de la cuarta nota: "))
-n5 = float(input("Digite el valor de la quinta nota: "))
+filas = int(input("Digite la cantidad de filas: "))
 
-if (n1 >5) or (n2>5) or (n3>5) or (n4>5) or (n5>5):
-    print("Error al digitar las notas")
-else:
-    total = (n1*0.15 + n2*0.2 + n3*0.15 + n4*0.3 + n5*0.2) 
-
-    if total < 2.0:
-        print("No puede habilitar")
-    elif total < 3.0:
-        print("Reprobó")
-    elif total >= 3.0:
-        print("Aprobó")
+for i in range (1,filas+1):
+    if i == 1:
+        p = str(i)
     else:
-        print("Felicitaciones por su desempeño académico.")
+        p = p +str(i)
+    print(p)
